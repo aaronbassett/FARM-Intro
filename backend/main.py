a6046399e264c95ac7e1e74321d67af284429c25
Aaron Bassett <me@aaronbassett.com>
2020-11-06 00:26:30 -0500

    Add Tasks CRUD API

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import uvicorn
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import settings
+
+from apps.todo.routers import router as todo_router
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
+    app.mongodb = app.mongodb_client[settings.DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+app.include_router(todo_router, tags=["tasks"], prefix="/task")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        reload=settings.DEBUG_MODE,
+        port=settings.PORT,
+    )
